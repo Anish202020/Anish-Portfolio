7a3c6216fdd6e6b74a6d575b4088187211154de2
Anish202020 <anishkumarbdmi@gmail.com>
2025-01-27 13:46:45 +0530

    commit changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -38,7 +38,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Anish Kumar
+          Copyright © 2025 Anish Kumar
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
